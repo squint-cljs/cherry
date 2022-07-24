e8253922558150be4a211f53853d3c67a4d3597e
Michiel Borkent <michielborkent@gmail.com>
2022-07-24 12:35:22 +0200

    yo

--- test/cherry/transpiler_test.cljs
+++ test/cherry/transpiler_test.cljs
@@ -1,10 +1,17 @@
 (ns cherry.transpiler-test
   (:require
    [cherry.transpiler :as cherry]
+   [clojure.string :as str]
    [clojure.test :as t :refer [deftest is]]))
 
-(defn eval! [expr]
-  (js/eval (doto (cherry/transpile-form expr) prn)))
+(defn js! [expr]
+  (let [js (cherry/transpile-form expr)]
+    [(js/eval js) js]))
+
+(deftest do-test
+  (let [[v s] (js! '(do 1 2 3))]
+    (is (= 3 v))
+    (is (not (str/includes? s "function")))))
 
 (deftest let-test
-  (is (= 3 (eval! '(let [x (do 1 2 3)] x)))))
+  (is (= 3 (first (js! '(let [x (do 1 2 3)] x))))))
