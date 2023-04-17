0dc734a2a45a821316d175335445b5e61ca5f9ad
Michiel Borkent <michielborkent@gmail.com>
2023-04-17 20:35:48 +0200

    embed: when ns already exists, still copy stuff

--- src/cherry/embed.clj
+++ src/cherry/embed.clj
@@ -7,15 +7,15 @@
         path-sym (symbol "path")]
     `(let [~path-sym (.split ~ns-str ".")
            ~ns-sym (cherry.embed/vbk js/globalThis ~path-sym)]
-       (when-not ~ns-sym
-         (let [~ns-sym (let [obj# (cljs.core/js-obj)]
-                         (cherry.embed/assoc-in! js/globalThis ~path-sym obj#)
-                         obj#)]
-           ~@(keep (fn [[k v]]
-                     (if (or (:fn-var v)
-                             (:type v))
-                       `(~'unchecked-set ~ns-sym
-                         (str (munge ~(str k))) ~(symbol (str sym) (str k)))
-                       nil #_(binding [*out* *err*]
-                               (prn v))))
-                   (ana/ns-publics sym)))))))
+       (let [~ns-sym (or ~ns-sym
+                         (let [obj# (cljs.core/js-obj)]
+                           (cherry.embed/assoc-in! js/globalThis ~path-sym obj#)
+                           obj#))]
+         ~@(keep (fn [[k v]]
+                   (if (or (:fn-var v)
+                           (:type v))
+                     `(~'unchecked-set ~ns-sym
+                       (str (munge ~(str k))) ~(symbol (str sym) (str k)))
+                     nil #_(binding [*out* *err*]
+                             (prn v))))
+                 (ana/ns-publics sym))))))
