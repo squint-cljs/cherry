e5ea0265654c8411fa66666dd909a567050cc404
Michiel Borkent <michielborkent@gmail.com>
2023-12-03 12:32:52 +0100

    Fix index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-export { compileString } from '../../../../var/folders/j9/xmjlcym958b1fr0npsp9msvh0000gn/T/!Users!borkdude!dev!cherry!lib!compiler.js~'
+export { compileString } from './lib/compiler.js';
