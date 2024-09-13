645c48cb00df978113cbe886e6000c21fa15508b
Michiel Borkent <michielborkent@gmail.com>
2024-09-13 15:32:08 +0200

    macro stuff

---
+++ src/cherry/compiler/sci.cljs
@@ -0,0 +1,25 @@
+(ns cherry.compiler.sci
+  (:require ["fs" :as fs]
+            [cherry.compiler.node :as cn :refer [sci]]
+            [sci.core :as sci]
+            [squint.internal.node.utils :refer [resolve-file]]))
+
+(defn slurp [f]
+  (fs/readFileSync f "utf-8"))
+
+(def ctx (sci/init {:load-fn (fn [{:keys [namespace]}]
+                               (let [f (resolve-file namespace)
+                                     fstr (slurp f)]
+                                 {:source fstr}))
+                    :classes {:allow :all
+                              'js js/globalThis}}))
+
+(sci/alter-var-root sci/print-fn (constantly *print-fn*))
+(sci/alter-var-root sci/print-err-fn (constantly *print-err-fn*))
+
+(sci/enable-unrestricted-access!)
+
+(defn init []
+  (reset! sci {:resolve-file resolve-file
+               :eval-form (fn [form _cfg]
+                            (sci/eval-form ctx form))}))
